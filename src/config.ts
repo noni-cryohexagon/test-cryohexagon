7d207fe9341e3a69d06087baff8569088e650841
noni <noni@cryohexagon.com>
2025-08-07 18:11:50 +0300

    deploy

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,13 @@
 if (!import.meta.env.VITE_SUPABASE_ANON_KEY) {
-  console.log("🚨 ", import.meta.env.VITE_SUPABASE_ANON_KEY);
-  console.log("🚨 ", import.meta.env.SUPABASE_ANON_KEY);
+  alert("🚨 ", import.meta.env);
+  alert("🚨 ", import.meta.env.VITE_SUPABASE_ANON_KEY);
+  alert("🚨 ", import.meta.env.SUPABASE_ANON_KEY);
   alert("VITE_SUPABASE_ANON_KEY is required");
-  throw new Error("VITE_SUPABASE_ANON_KEY is requireddddd");
+  throw new Error("VITE_SUPABASE_ANON_KEY is requiredddddcccc");
 }
 if (!import.meta.env.VITE_SUPABASE_URL) {
   alert("VITE_SUPABASE_URL is required");
-  throw new Error("VITE_SUPABASE_URL is requireddddd");
+  throw new Error("VITE_SUPABASE_URL is requiredddddcccc");
 }
 
 console.log(import.meta.env.VITE_SUPABASE_ANON_KEY);
