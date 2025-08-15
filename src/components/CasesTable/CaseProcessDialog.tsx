2db2bf00ff2b45d802f3e029c78f209570569152
noni <noni@cryohexagon.com>
2025-08-15 15:22:10 +0300

    merge and fix

--- src/components/CasesTable/CaseProcessDialog.tsx
+++ src/components/CasesTable/CaseProcessDialog.tsx
@@ -57,7 +57,7 @@ const stepsMap = {
     isShowFooter: false,
   },
   organizeSamples: {
-    nextStep: "finalConfirmationStep", //"allocateCanesStep",
+    nextStep: "allocateCanesStep",
     isShowFooter: true,
     footerTitle: "Organize samples in straws.",
     footerDescription: "Select samples to create a straw.",
